3ad5069b94ac51b9d6f65bb327b20b07d3d18a30
unknown <MikeKnoop@.(none)>
2011-09-23 03:25:25 -0500

    fixing footer images

--- www/module/footer_images.php
+++ www/module/footer_images.php
@@ -2,19 +2,11 @@
 
 $pictures = $GLOBALS['Photo']->GetRandomFooter();
 
-if (is_array($pictures))
-{
-  foreach ($pictures as $picture)
-  {
-    echo '
-          <a target="_new" href="'.$picture['link'].'">
-            <div class="footer_image_single_container">
-              <img class="footer_image" src="'.$picture['picture'].'" />
-            </div>
-          </a>
-          
-          ';
+if (is_array($pictures)) {
+  foreach ($pictures as $picture) {
+		if (!empty($picture)) {
+			echo "<a target=\"_new\" href=\"{$picture['link']}\"><div class=\"footer_image_single_container\"><img class=\"footer_image\" src=\"{$picture['picture']}\"></img></div></a>";
+    }
   }
 }
-
 ?>
