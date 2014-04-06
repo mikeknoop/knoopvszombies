ece130554f2d6e73cecc9c5cc5ba65ff8e3e36f7
Lars Paulson <larspcus2@gmail.com>
2014-04-06 14:25:53 -0700

    Update rules page with spring 2014 rules

--- www/rules.php
+++ www/rules.php
@@ -51,15 +51,25 @@
                  
           <div class="odd">
             <div class="rules_header">Game <span class="accent_color">Rules</span></div>
-            <div>(last update, September 29, 2013)</div>
-            <div>Humans vs. Zombies (HvZ) is a game of tag of epic proportions. There are two teams, the Human Resistance, and the Zombie Horde. All players begin as Humans, except one who is randomly chosen to be the Original Zombie (Players can opt out of being chosen as the Original Zombie). The Original Zombie tags Human players, turning them into Zombies. The Zombies must "feed" on a Human every 48 hours, by tagging them, or they starve to death and are out of the game. The Humans can defend themselves from the Zombies with various game Nerf devices, such as Nerf blasters.</div>
+            <div>(last update: April 6, 2013)</div>
+            <div>Humans vs. Zombies (HvZ) is a game of tag of epic proportions. There are two teams, the Human Resistance, and the Zombie Horde. All players begin as Humans, except one who is randomly chosen to be the Original Zombie (Players can opt out of being chosen as the Original Zombie). The Original Zombie tags Human players, turning them into Zombies. The Zombies must "feed" on a Human every 60 hours, by tagging them, or they starve to death and are out of the game. The Humans can defend themselves from the Zombies with various game approved equipment stated below.</div>
           </div>
 
           <div class="even">
 
             <div><span class="rules_heading">New Rules and Rule Changes for this semester</span></div>
              <div><OL class="rules">
-                <LI>No new rules yet for this semester.</LI>
+                <LI>Starve timers are now set to 60 hrs, each "kill" counts for 60 hrs
+                <LI>Remote controlled nerf blasters or equipment are not allowed to be used during the game
+                <LI>Kill sharing has new parameters
+                <LI>Doorways:
+                <OL class="rules">
+                <LI>A human is safe if they have at least one foot within the doorway
+                <LI>A human is safe if, as they are entering the building, they have one hand on the door handle
+                <LI>If the door is locked, then you are only safe if you are able to open the door
+                <LI>Zombies are never safe in doorways
+                </OL>
+
              </OL></div>
   
               
@@ -67,15 +77,21 @@
           
           <div class="odd">
              <div><span class="rules_heading">Objectives</span></div>
-             <div> The Zombies win when all Human players have been tagged and turned into Zombies, or by preventing the Humans from completing the final mission.</div>
-             <div> The Humans win by surviving long enough for all of the Zombies to starve, or by completing the final mission.</div>
+             <div> Zombies: win when all Human players have been tagged and turned into Zombies, or by preventing the Humans from completing the final mission.</div>
+             <div> Humans: win by surviving long enough for all of the Zombies to starve, or by completing the final mission.</div>
           </div>
           
           <div class="even">
             <div><span class="rules_heading">Eligibility</span></div>
-            <div>To be eligible to play, players must be able to spend an adaquate amount of time on campus (1-3hrs per day) during the game week. Violation of any rules may result in a ban from the game. You must register at the OSU HVZ website to play. You must also agree to the OSU OSUndead Humans vs. Zombies Agreement (when you register on this website) and complete, sign, and return the liability waiver at orientation.  You MUST attend ONE orientation out of the dates specified on the front page, and if you cannot make one of these dates email us at mods@osundead.com to arrange a separate one.</div>
-            <div>You will start as deceased if you do not attend an orientation AND return the liability waiver.  (Plus, you won't get an armband if you don't.)</div>
-            <div>One-on-one orientations can be done before the start of the game on request by emailing the Mod email at Mods@osundead.com</div>
+            <div><OL class="rules">
+               <LI>To be eligible to play, players must be able to spend an adequate amount of time on campus (1-3hrs per day) during the game week.
+               <LI>Violation of any rules may result in a ban from the game.
+               <LI>You must register at the OSU HVZ website to play.
+               <LI>You must also agree to the OSU OSUndead Humans vs. Zombies Agreement (when you register on this website) and complete, sign, and return the liability waiver at orientation. 
+               <LI>You MUST attend ONE orientation out of the dates specified on the front page, and if you cannot make one of these dates email us at mods@osundead.com to arrange a separate one.
+               <LI>You will start as deceased if you do not attend an orientation AND return the liability waiver. (Plus, you won't get an armband if you don't.)
+               <LI>One-on-one orientations can be done before the start of the game on request by emailing the Mod email at Mods@osundead.com
+            </OL></div>
           </div>
 
           <div class="odd">
@@ -88,7 +104,13 @@
                 <LI>Nerf markers must not hurt on impact. (If you're not sure about this, ask a moderator).
                 <LI>Common Sense. If something seems excessively dangerous, don't do it.
                 <LI>Stairs are safe for humans only. Zombies can be stunned on stairs, humans can not be tagged as long as they have at least one foot on the stairs. To count as stairs, there must be three vertical faces.
-                <LI>With doorways, if a human has at least one foot inside a door he/she is safe, however a zombie must have both feet inside the doorway to be considered safe.
+                <LI>Doorways:
+                <OL class="rules">
+                <LI>A human is safe if they have at least one foot within the doorway
+                <LI>A human is safe if, as they are entering the building, they have one hand on the door handle
+                <LI>If the door is locked, then you are only safe if you are able to open the door
+                <LI>Zombies are never safe in doorways
+                </OL>
                 <LI>Vehicles: Players on/in vehicles are outside of the game. They cannot be attacked, and cannot attack.  Players on roller blades are always safe because a push will cause them to move. (Please don't ride bikes, skateboards, or rollerblades at missions. You may use them to get to and from the mission location, but using them during the mission falls under DBAD).
              </OL></div>
 
@@ -106,10 +128,10 @@
             
           
           <div class="odd">
-            <div><span class="rules_heading">SAFE ZONES</span></div>
+            <div><span class="rules_heading">Safe Zones</span></div>
             <div>Players that are in safe zones cannot be shot or tagged in these places. Human players can however fire from within a safe zone to clear exits.</div>
             <div><OL class="rules">
-              <LI>Residence Halls. Humans may not shoot out of windows or from fire escapes, either.
+              <LI>Residence Halls. Humans may not shoot out of windows or from fire escapes.
               <LI>Bathrooms
               <LI>All Buildings (four walls and a ceiling)
               <LI>Stairs are safe for humans only.
@@ -144,8 +166,6 @@
           <div class="odd">
             <div><span class="rules_heading">Locations</span></div>
             <div>Areas on campus that are safe to play in and not listed as a Safe Zone or No-Play Zone are fair game.</div>
-            <div>Staying on campus: OSU has been quarantined by the international community due to a zombie outbreak, and as such Humans must stay in or near the play area. If you need to leave campus for longer than 48 hours, you are considered rescued, and are out of the game.</div>
-                
           </div>
 
           <div class="even">
@@ -159,7 +179,7 @@
             <div>When Tagged By a Zombie: When tagged by a Zombie, a Human is required to give the Zombie one of their ID cards, which the Zombie will use to register the tag.  The player is now a Stunned Zombie and will become an unstunned Zombie fifteen minutes after being tagged.</div>
             <div>Wearing an Armband: Humans must wear provided bandanna around their bicep to identify at all times that they are in a game-on zone to identify them as Humans players in the game. (This will come in handy when you become a Zombie!) Zombies must wear the bandanna on their head, OVER hoodies, hats, hair, etc. so that it is visible at all times from all angles.</div>
             <div>Time keeping: Players must be able to keep track of time, such as with a watch or cell phone.</div>
-            <div>Zombies must "feed" every 48 hours. A Zombie feeds by reporting their tag to the website, within 2 hours of the tag. If a Zombie does not feed, every 48 hours, they "starve", and are out of the game. When a Zombie feeds, this 48 hour timer is reset.</div>
+            <div>Zombies must "feed" every 60 hours. A Zombie feeds by reporting their tag to the website, within 2 hours of the tag. If a Zombie does not feed, every 60 hours, they "starve", and are out of the game. When a Zombie feeds, this 60 hour timer is reset.</div>
             <div>Wearing Your Headband: Zombies must wear the bandanna around their head at all times while playing. Again, make sure it is over hats, hoods, hair, etc.</div>
             <div>A tag is a firm touch to a Human. (Keep in mind where an appropriate place to tag a Human is. Use good judgment; any violation of this rule is against game policy as well as being incredibly disrespectful, and will result in removal from the game at the very least). After tagging a Human the Zombie must collect one of the Human's ID card, and must report their tag to get credit for feeding.</div>
             <div>Getting Tagged: When hit with a Human Nerf device, a Zombie is stunned for 15 minutes. A stunned Zombie may not interact with the game in any way. This includes shielding other Zombies from bullets, continuing to run toward a Human, or providing information to the Horde. If shot while already stunned, the Zombie's stun timer is not reset back to 15 minutes.</div>
@@ -187,7 +207,7 @@
         
             <div>Nerf devices must not look realistic. Federal law requires that toy blasters have a brightly colored tip. We require blasters to be obviously brightly colored - if we think it's too realistic looking, it can't be used. If you're in doubt, ask a mod ahead of time. Please don't paint blasters completely black or other colors found on realistic weaponry. We'll have spray-paint at the orientations to help you make sure your nerf blasters meet standards. All devices except socks and marshmallows must be approved by a Moderator prior to use. Approved devices will be marked as such by a zip-tie.  Projectiles must not hurt on impact.</div>
             <div>Nerf blasters cannot fire anything except foam projectiles such as darts or balls. Socks must be clean. Socks must be thrown; they can be modified, but only by adding more socks, not by cutting or by adding anything but socks. Marshmallows must be thrown and cannot shot with blow guns.
-</dev>
+</div>
             <div>Only commercially available, off-the-shelf darts may be used in nerf blasters. For safety reasons, darts may not be modified to affect their flight performance in any way.</div>
             <div>All ammunition must be cleaned up by whoever fired it. Don't leave darts and marshmallows and socks lying around. Be respectful of campus - ours is a beautiful one, let's keep it that way.</div>
             <div>If a moderator says a Nerf device is not allowed, then it is not allowed. Moderators do not need to cite a rule to ban Nerf devices. If you have a problem with this, you can appeal to another moderator.</div>
