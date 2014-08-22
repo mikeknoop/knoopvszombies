00d0215bb8ff3ee5fe21f6372e8348d8c1e458d8
Lars Paulson <larspcus2@gmail.com>
2014-08-21 17:15:47 -0700

    remove unused pages

--- www/fb/hvzmizzou/forum.php
+++
@@ -1,3 +0,0 @@
-<div style="margin: 50px 0; font-size: 18px; color: #222222; text-align: center;">
-Our Discussion Boards have moved! They are now part of muzombies.org. <a href="http://www.muzombies.org/forums">Click here to visit them</a>.
-</div>
